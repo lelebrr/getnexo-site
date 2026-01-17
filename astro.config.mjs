9477cb5aa4bd612529e7ce2023b35730afdffbbd
Leandro <lele@getnexo.com.br>
2026-01-17 02:01:51 -0300

    corrigindo astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,2 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-    site: 'https://getnexo.com.br',
-    integrations: [
-        react(),
-        tailwind(),
-        sitemap(),
-        partytown({
-            config: { forward: ['dataLayer.push'] }
-        })
-    ],
-    compressHTML: true,
-    vite: {
-        build: {
-            minify: 'esbuild',
-            sourcemap: false,
-            cssCodeSplit: true,
-        }
-    }
-});
+export default defineConfig({});
