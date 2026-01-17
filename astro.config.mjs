e0c767caa7740b3d332dff488712817f13c1752f
Leandro <lele@getnexo.com.br>
2026-01-17 12:39:44 -0300

    feat: Universal Adapter (Vercel + Docker Support)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
+const isVercel = process.env.VERCEL === '1';
+
 export default defineConfig({
     output: 'server',
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
+    adapter: isVercel
+        ? vercel({ webAnalytics: { enabled: true } })
+        : node({ mode: 'standalone' }),
     site: 'https://getnexo.com.br/',
     integrations: [
         react(),
